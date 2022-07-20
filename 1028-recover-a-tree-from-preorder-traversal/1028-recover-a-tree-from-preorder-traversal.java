78f4be296951cbb4cf4004e21ba1578bff833435
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-20 21:18:27 +0530

    Time: 18 ms (16.67%), Space: 46.8 MB (35.91%) - LeetHub

---
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Pair {
+    TreeNode node;
+    int dep;
+    
+    Pair(TreeNode node, int d){
+        this.node = node;
+        dep = d;
+    }
+        
+}
+class Solution {
+    public TreeNode recoverFromPreorder(String s) {
+       Stack<Pair> st = new Stack<>();
+        TreeNode res = null;
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '-'){
+                count++;
+            }
+            else{
+                int start = i;
+                while(i < s.length() && s.charAt(i) != '-') i++;
+                
+                String str = s.substring(start,i);
+                i--;
+                int val = Integer.parseInt(str);
+                TreeNode temp = new TreeNode(val);
+                if(count == 0) res = temp;                
+                Pair p = new Pair(temp,count);
+                
+                
+                while(!st.isEmpty() && count != st.peek().dep+1){
+                    st.pop();   
+                }             
+                if(!st.isEmpty() && st.peek().node.left == null){
+                    st.peek().node.left = temp;
+                }
+                else if(!st.isEmpty()){
+                    st.peek().node.right = temp;
+                }
+                count = 0;
+                 st.push(p); 
+                
+            }
+        }
+
+       return res;
+    }
+}
