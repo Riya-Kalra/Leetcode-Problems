cf78e8b5e3c1ea1d2ebf8ae825c4490d7fd03152
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-22 01:06:03 +0530

    Time: 77 ms (42.58%), Space: 54.5 MB (42.92%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -5,27 +5,24 @@ class Solution {
         for(int n:nums)
             sum+=n;
         if(sum%2==1) return false;
-        t= new int [nums.length][(sum/2)+1];
-        for(int i=0;i<nums.length;i++)
-            Arrays.fill(t[i],-1);
-        return solve(nums.length-1,sum/2, nums);
+        
+        int target=sum/2;
+        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+        for(int i = 0; i < dp.length; i++) {
+            for(int  j = 0; j < dp[0].length; j++) {
+                if(i == 0 && j == 0) { //empty ss{} sum will be 0
+                    dp[i][j] = true;
+                } else if(i == 0) { //empty ss{} sum cannot be greater than 0
+                    dp[i][j] = false;
+                } else if(j == 0 || dp[i - 1][j]) { 
+                    dp[i][j] = true;
+                } else if(j >= nums[i - 1]) { 
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]]; 
+                }
+            }
+        }
+        return dp[nums.length-1][sum/2];
         
     }
-    public boolean solve (int i,int target,int[] nums){
-        if(target ==0 ) return true;
-        if(i==0){
-            if(nums[i]==target) 
-                return true;
-            else
-                return false;
-        }
-        if (t[i][target]!=-1)
-            return t[i][target]==0? false: true;
-        boolean notPick = solve(i-1,target, nums);
-        boolean pick=false;
-        if(nums[i]<=target) 
-            pick= solve(i-1, target-nums[i], nums);
-        t[i][target]=(pick|notPick)== false ? 0:1 ;
-        return t[i][target]==0? false: true;
-    }
+    
 }
