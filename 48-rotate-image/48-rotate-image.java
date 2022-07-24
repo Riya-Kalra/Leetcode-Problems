6d52709977a5fefeb9688a8d7286e3ee3ea6718c
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-24 17:59:43 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (81.94%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n=mat.length;
+        
+       for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                    int t=mat[i][j];
+                    mat[i][j]=mat[j][i];
+                    mat[j][i]=t;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <n/2; j++) {
+                    int t=mat[i][j];
+                    mat[i][j]=mat[i][n-j-1];
+                    mat[i][n-1-j]=t;
+            }
+        }
+    }
+}
