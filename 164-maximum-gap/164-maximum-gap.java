cd56c4170828b50f9d79cefc5963757fad4cc19d
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-26 22:47:36 +0530

    Time: 103 ms (13.37%), Space: 82 MB (43.51%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.java
+++ 164-maximum-gap/164-maximum-gap.java
@@ -3,49 +3,35 @@ class Solution {
         int n=nums.length;
         if(n<2) return 0;
          int max=nums[0]; 
-		for(int i=1;i<nums.length;i++)
+		for(int i=1;i<n;i++)
 		 { if(nums[i]>max)
 			 max=nums[i];
 		 }
 		
-	      int res[]=new int[nums.length]; //result array
-	    
-	      //Arrays.fill(count, 0);
+	      int res[]=new int[n];
 	      int exp= 1; //10,100,1000... to scan the digits from lsb to msb
-	      
-      while(max / exp > 0)  //when this becomes 0,means we have move ahead of msb now
-      {  int count[]=new int[10]; 
-	      for(int i=0;i<nums.length;i++)
-	      {
-	    	  
-	    	  count[(nums[i] /exp) % 10]++;  //incrementing the count array
-	      }
-	       
-	      for(int i=1;i<10;i++)
-	      {  
-	    	  count[i]+=count[i-1];   //cumulative frequency count array
-	      }
-	      
-	      for(int i=nums.length-1;i>=0;i--)  //start looping from the end of array
-	      {
-	    	  res[count[(nums[i]/exp)%10]-1]=nums[i];   
-	    	  count[(nums[i]/exp)%10]--;     //decrementing frequency in count array
-	      }
-	      
-	      for(int i=0;i<nums.length;i++)
-	      {
-	    	   nums[i]=res[i];    //for the next pass resultant array will be input
-	      }
-	      exp*=10;
-      }
-        
-        int m=0;
-        for(int i=1;i<nums.length;i++)
-        {
-            m=Math.max(m,nums[i]-nums[i-1]);
+        while(max/exp>0){
+            int cnt[]=new int[10];
+            for(int i=0;i<n;i++)
+	    	  cnt[(nums[i] /exp) % 10]++; //to get the digit 
+            for(int i=1;i<10;i++)
+                cnt[i]+=cnt[i-1];
+            for(int i=n-1;i>=0;i--){
+                res[cnt[(nums[i]/exp)%10]-1]=nums[i];
+                cnt[(nums[i]/exp)%10]--;
+            }
+	        for(int i=0;i<n;i++){
+                nums[i]=res[i];
+            }
+	       exp*=10;
+            
         }
-        return m;
-	  
-        
+	      int diff=0;
+        for(int i=1;i<n;i++)
+        {
+            diff=Math.max(diff,nums[i]-nums[i-1]);
+        }
+        return diff;
+    
     }
 }
