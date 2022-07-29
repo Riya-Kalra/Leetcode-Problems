8de0f09abdae9b2f06efca586236ecbfbf7317e4
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-29 15:24:18 +0530

    Time: 44 ms (30.82%), Space: 58.8 MB (9.74%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,27 +1,19 @@
 class Solution {
-    
-    public int coinChange(int[] coins, int a) {
-        int prev[]= new int[a+1];
-        int dp[]= new int[a+1];
-        for(int j=0;j<=a;j++){
-            if(j%coins[0]==0)
-                prev[j]=j/coins[0];
-            else
-                prev[j]=Integer.MAX_VALUE-1;
-        }
-        for(int i=1;i<coins.length;i++){
-            for(int j=0;j<=a;j++){
-                int notTake=prev[j];
-                int Take=Integer.MAX_VALUE-1;
-                if(coins[i]<=j)
-                    Take=1+dp[j-coins[i]];
-                
-                dp[j]=Math.min(Take,notTake);  
+    public int coinChange(int[] coins, int amount) {
+        int c=coins.length;
+        int dp[][]=new int [c+1][amount+1];
+        for(int i=0;i<=c;i++){
+            for(int j=0;j<=amount;j++){
+                if (j == 0)
+                    dp[i][j] =  0;
+                else if (i==0)
+                    dp[i][j] =Integer.MAX_VALUE-1;
+                else if (coins[i-1]<=j)
+                    dp[i][j]= Math.min(0 + dp[i - 1][j], 1 + dp[i][j - coins[i - 1]]);
+                else
+                    dp[i][j]=0 + dp[i - 1][j];
             }
-            prev=dp;
         }
-        int ans=prev[a];
-        if(ans==Integer.MAX_VALUE-1) return -1;
-        return ans;
+        return dp [c][amount]>=Integer.MAX_VALUE-1?-1:dp [c][amount];
     }
 }
