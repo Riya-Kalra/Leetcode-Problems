5f097925ded32d110a3fb1f4783eda6346405406
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-29 14:54:32 +0530

    Time: 1 ms (98.01%), Space: 42.4 MB (85.88%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> findAndReplacePattern(String[] words, String pattern) {
+        List<String> ans = new ArrayList();
+        for (String word: words)
+            if (match(word, pattern))
+                ans.add(word);
+        return ans;
+    }
+    public boolean match(String word, String pattern) {
+        Map<Character, Character> M = new HashMap();
+        for (int i = 0; i < word.length(); ++i) {
+            char w = word.charAt(i);
+            char p = pattern.charAt(i);
+            if (!M.containsKey(w)) M.put(w, p);
+            if (M.get(w) != p) return false;
+        }
+         boolean[] seen = new boolean[26];//To check if same pattern character is mapped with 2 diff word character
+        for (char p: M.values()) {
+            if (seen[p - 'a']) return false;
+            seen[p - 'a'] = true;
+        }
+       
+        return true;
+    }
+}
