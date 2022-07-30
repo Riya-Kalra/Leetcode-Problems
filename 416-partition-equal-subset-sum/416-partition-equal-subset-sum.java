1459c4b214d204ef0dbe42aa9d82df922a4b2c41
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-30 09:12:56 +0530

    Time: 137 ms (20.47%), Space: 68.1 MB (24.18%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,28 +1,33 @@
 class Solution {
-    int t[][];
+    int dp[][];
     public boolean canPartition(int[] nums) {
         int sum=0;
-        for(int n:nums)
+        for(int n: nums){
             sum+=n;
-        if(sum%2==1) return false;
-        
-        int target=sum/2;
-        boolean[][] dp = new boolean[nums.length + 1][target + 1];
-        for(int i = 0; i < dp.length; i++) {
-            for(int  j = 0; j < dp[0].length; j++) {
-                if(i == 0 && j == 0) { //empty ss{} sum will be 0
-                    dp[i][j] = true;
-                } else if(i == 0) { //empty ss{} sum cannot be greater than 0
-                    dp[i][j] = false;
-                } else if(j == 0 || dp[i - 1][j]) { 
-                    dp[i][j] = true;
-                } else if(j >= nums[i - 1]) { 
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]]; 
-                }
-            }
         }
-        return dp[nums.length-1][sum/2];
-        
+        if(sum%2==1)
+            return false;
+        dp= new int[nums.length][sum/2+1];
+        for(int i=0;i<nums.length;i++)
+            Arrays.fill(dp[i],-1);
+        return solve(0,nums,sum/2);
     }
-    
+    public boolean solve(int i, int []nums, int target){
+        if(target==0)
+                return true;
+        if(i==nums.length){
+            if(target==0)
+                return true;
+            else
+                return false;
+        }
+        if(dp[i][target]!=-1) 
+            return dp[i][target]==0? false :true;
+        boolean notTake=solve(i+1,nums,target);
+        boolean take=false;
+        if(nums[i]<=target)
+            take=solve(i+1,nums,target-nums[i]);
+        dp[i][target]= (take|notTake)==true?1:0;
+        return take|notTake;
+    }
 }
