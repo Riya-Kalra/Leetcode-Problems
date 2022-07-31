bd81b634483b4e9a37d0c18182bdc307e685cebe
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-31 18:38:43 +0530

    Time: 218 ms (20.00%), Space: 154.9 MB (60.00%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -6,26 +6,21 @@ class Solution {
         Arrays.fill(vis,-1);
         for(int i=0;i<n;i++){
             if(vis[i]==-1){
-                HashMap<Integer,Integer> dfsvis=new HashMap<>();
+               HashMap<Integer,Integer> dfsvis=new HashMap<>();
                 
                 dfs(edges,i,vis,0,dfsvis);
             }
         }
         return cycle;
     }
-    public void dfs(int edges[],int i,int vis[],int c,HashMap<Integer,Integer> currVis){
-        
-        vis[i] = 1;
-        currVis.put(i,c);
-        // go to adjacent node...
-        int ele = edges[i];
-        if(ele == -1) return;
-        if(currVis.containsKey(ele)){ // already visited and cycle..
-            cycle = Math.max(cycle, (c+1)-currVis.get(ele));
+    public void dfs(int edges[],int i,int vis[],int c,HashMap<Integer,Integer> dfsvis){
+       if(i==-1) return;
+        if(dfsvis.containsKey(i))
+            cycle=Math.max(cycle,c-dfsvis.get(i));
+        else if(vis[i] == -1){
+            dfsvis.put(i,c);
+            vis[i]=1;
+            dfs(edges,edges[i],vis,c+1,dfsvis);
         }
-        else if(vis[ele] == -1){
-            dfs(edges,ele, vis,c+1,currVis);
-        }     
-        
     }
 }
