158a7ee4cf4e2c4c2864a169a1ec12951043fd71
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-07-31 23:45:11 +0530

    Time: 17 ms (98.93%), Space: 54.2 MB (75.05%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.java
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -1,18 +1,49 @@
+class Pair{
+    int start;
+    int end;
+    public Pair(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+}
+class Node{
+    Pair p;
+    Node left;
+    Node right;
+    public Node(){};
+    public Node(Pair p, Node left, Node right){
+        this.p = p;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 class MyCalendar {
-    TreeMap<Integer, Integer> cal;
+    Node root;
     public MyCalendar() {
-         cal = new TreeMap();
+      root= new Node();  
     }
     
     public boolean book(int start, int end) {
-        Integer prev=cal.floorKey(start);
-        Integer next=cal.ceilingKey(start);
-        if((prev==null || cal.get(prev)<=start)&&(next==null || end<=next)){
-            cal.put(start,end);
+       return checkAndUpdate(start, end, root); 
+    }
+    
+    public boolean checkAndUpdate(int start, int end, Node root){
+        if(root.p == null){
+            root.p = new Pair(start, end);
+            root.left = new Node();
+            root.right = new Node();
             return true;
         }
-           return false;
-    }
+    
+        if(end <= root.p.start){
+            return checkAndUpdate(start, end, root.left);
+        }
+        if(start >= root.p.end){
+            return checkAndUpdate(start, end, root.right);
+        }
+        return false;
+}
 }
 
 /**
