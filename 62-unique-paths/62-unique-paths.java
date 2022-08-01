9cd344bc3784d16a3811be65d622b950a20bea3d
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-01 09:26:54 +0530

    Time: 1 ms (55.39%), Space: 41.7 MB (10.24%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,18 +1,21 @@
 class Solution {
-    HashMap<String,Integer> map =new HashMap<>();
+    int count=0;
+    int dp[][];
     public int uniquePaths(int m, int n) {
-        if(n == 0 || m == 0){
-            return 0;      
+        dp= new int[m][n];
+        for(int i=0;i<m;i++)
+        Arrays.fill(dp[i],-1);
+        
+        return dfs(0,0,m,n);
+    }
+    public int dfs(int i,int j,int m,int n){
+        if(i==m-1 && j==n-1){
+            return 1;
         }
-        if(n == 1 && m == 1){
-            return 1;      
-        }
-        String cell = new String(m + "," + n);
-        if(map.containsKey(cell)) 
-            return map.get(cell);
-        int down = uniquePaths(m-1,n);
-        int right = uniquePaths(m,n-1);
-        map.put(cell,right+down);
-        return right+down;
+        if(i<0 ||j<0 ||i>=m|| j>=n)
+            return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+         dp[i][j]=dfs(i+1,j,m,n) +dfs(i,j+1,m,n);
+        return dp[i][j];
     }
 }
