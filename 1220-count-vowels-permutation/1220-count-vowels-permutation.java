33f7438729ac1632db0969f920f3098005b9b2f1
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-07 12:01:24 +0530

    Time: 34 ms (44.22%), Space: 53.8 MB (32.05%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,35 +1,30 @@
 class Solution {
-    char ch[]={'a','e','i','o','u'};
-    long count=0l;
-    int mod=1000000007;
-   HashMap<String,Long> map =new HashMap<>();
+    
+    int MOD=1000000007;
+ 
     public int countVowelPermutation(int n) {
-        if(n==1) return 5;
-        for(char c:ch){
-            count+=dfs(c,n - 1)%mod;
+        long dp[][]=new long[n+1][5];
+        for(int i = 0; i < 5; i++)
+        dp[1][i] = 1L;
+    
+        for(int i = 1; i < n; i++)
+        {
+            dp[i+1][0] = (dp[i][1] + dp[i][2] + dp[i][4]) %MOD;
+
+            dp[i+1][1] = (dp[i][0] + dp[i][2]) % MOD;
+
+            dp[i+1][2] = (dp[i][1] + dp[i][3]) % MOD;
+
+            dp[i+1][3] = dp[i][2];
+
+            dp[i+1][4] = (dp[i][2] + dp[i][3]) % MOD;
         }
-        //System.out.println(map.toString());
-        return (int)(count%1000000007);
-    }
-    public long dfs(char c, int n){
-        if(n==0){
-            return 1L;
+
+        long res = 0;
+        for(int i = 0; i < 5; i++)
+            res = (res + dp[n][i]) % MOD;
+
+        return (int)res;
         }
-        if(map.containsKey(c+" "+n))
-            return map.get(c+" "+n);
-        long ans=0L,result=0L;
-            if(c=='a')
-                ans=dfs('e',n-1);
-            else if (c=='e' )
-                ans=dfs('a',n-1)+dfs('i',n-1);
-            else if(c=='i')
-                ans=dfs('e',n-1)+dfs('a',n-1)+dfs('o',n-1)+dfs('u',n-1);
-            else if(c=='o' )
-                ans=dfs('u',n-1)+dfs('i',n-1);
-            else if(c=='u')
-                ans=dfs('a',n-1);
-        map.put(c+" "+n,ans%mod);
-        //System.out.println(ans + " " + c + " " + n);
-        return ans%mod;
-    }
+    
 }
