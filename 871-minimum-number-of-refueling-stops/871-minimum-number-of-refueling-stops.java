31807d83aa57dac9fa5687718ecbdd5d5da3162f
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-09 21:03:41 +0530

    Time: 6 ms (54.74%), Space: 48.8 MB (68.13%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.java
@@ -2,14 +2,10 @@ import java.util.*;
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         PriorityQueue<int[]> pq =new PriorityQueue<>((a,b)->(b[1]-a[1]));
-        int start=0, end=startFuel, cnt=0;
+        int start=0, end=startFuel, cnt=0,i=0,n=stations.length;
         while(end<target){
-            for(int i=0;i<stations.length;i++){
-                //System.out.println(stations[i][0]+" "+end);
-                if(stations[i][0]<=end && stations[i][0]>=0 ){
-                    pq.add(stations[i]);
-                    stations[i][0]=-1;
-                }
+           while (i < n && stations[i][0] <= end) {
+                pq.offer(stations[i++]);
             }
             //System.out.println(Arrays.deepToString(stations));
             if(pq.isEmpty())
