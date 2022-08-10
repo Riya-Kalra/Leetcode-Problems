27ff19f52884bd570b3bd4daa8104a08e6b7ae0b
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-10 16:32:11 +0530

    Time: 7 ms (78.65%), Space: 56.1 MB (32.29%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.java
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.java
@@ -3,15 +3,17 @@ class Solution {
         int n=nums.length;
         Arrays.sort(nums);
         int copy[]=nums.clone();
-        int mid=(n)/2,l=0,index=1;
-        for(int i = nums.length - 1; i > (nums.length - 1) / 2; i--){
+        int mid=(n-1)/2,l=0,index=1;
+        for(int i = n - 1; i > mid; i--){
             nums[index] = copy[i];
             index += 2;
         }
         index = 0;
-        for(int i = (nums.length - 1) / 2; i >= 0; i--){
+        for(int i = mid; i >= 0; i--){
             nums[index] = copy[i];
             index += 2;
         }
     }
 }
+//nums=123456
+//ans=362514
