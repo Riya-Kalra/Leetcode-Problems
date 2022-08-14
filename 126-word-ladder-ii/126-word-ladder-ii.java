c2cc6ca9ac4bbb77353f1b43522f6966adfd9d67
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-14 22:16:17 +0530

    Time: 33 ms (36.94%), Space: 54.4 MB (17.53%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.java
+++ 126-word-ladder-ii/126-word-ladder-ii.java
@@ -1,44 +1,64 @@
 class Solution {
     List<List<String>> result = new ArrayList<>();
     Map<String, List<String>> adj = new HashMap<>();
+    
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        
         Set<String> set = new HashSet<>();
+        
         for(String a : wordList){
             set.add(a);
         } 
+        
         bfs(beginWord, endWord, set);
+        
         List<String> current = new ArrayList<>();
         current.add(endWord);
-        
         backtrack(endWord, beginWord, current);
+        
         return result;
     }
+    
     public void bfs(String beginWord, String endWord, Set<String> set){
+        
         Queue<String> queue = new LinkedList<>();
         queue.add(beginWord);
+        
         if(set.contains(beginWord)){
             set.remove(beginWord);
         }
-        Map<String, Integer> isEnqueued = new HashMap<>();
-        isEnqueued.put(beginWord, 1);
+        
+        HashSet<String> isEnqueued = new HashSet<>();
+        isEnqueued.add(beginWord);
+        
         while(queue.size() > 0){
+            
             List<String> visited = new ArrayList<>();
+            
             for(int i = queue.size() - 1; i >= 0; i--){
+                
                 String currentWord = queue.poll();
                 List<String> neighbours = transformed(currentWord, set);
+                
                 for(String word : neighbours){
+                    
                     visited.add(word);
+                    
                     if(!adj.containsKey(word)){
                         adj.put(word, new ArrayList<>());
                     }
+                    
                     adj.get(word).add(currentWord);
-                    if(!isEnqueued.containsKey(word)){
-                        isEnqueued.put(word, 1);
+                    
+                    if(!isEnqueued.contains(word)){
+                        isEnqueued.add(word);
                         queue.add(word);
                     }
                 }
             }
+            
             for(int i = 0; i < visited.size(); i++){
+                
                 if(set.contains(visited.get(i))){
                     set.remove(visited.get(i));
                 }
