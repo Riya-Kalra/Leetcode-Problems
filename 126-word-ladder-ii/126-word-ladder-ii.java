909580ec5a4f143c6ecc22035b6078f3c4bbdeb1
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-14 22:08:16 +0530

    Time: 24 ms (60.17%), Space: 54.2 MB (19.04%) - LeetHub

---
+++ 126-word-ladder-ii/126-word-ladder-ii.java
@@ -0,0 +1,79 @@
+class Solution {
+    List<List<String>> result = new ArrayList<>();
+    Map<String, List<String>> adj = new HashMap<>();
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        Set<String> set = new HashSet<>();
+        for(String a : wordList){
+            set.add(a);
+        } 
+        bfs(beginWord, endWord, set);
+        List<String> current = new ArrayList<>();
+        current.add(endWord);
+        
+        backtrack(endWord, beginWord, current);
+        return result;
+    }
+    public void bfs(String beginWord, String endWord, Set<String> set){
+        Queue<String> queue = new LinkedList<>();
+        queue.add(beginWord);
+        if(set.contains(beginWord)){
+            set.remove(beginWord);
+        }
+        Map<String, Integer> isEnqueued = new HashMap<>();
+        isEnqueued.put(beginWord, 1);
+        while(queue.size() > 0){
+            List<String> visited = new ArrayList<>();
+            for(int i = queue.size() - 1; i >= 0; i--){
+                String currentWord = queue.poll();
+                List<String> neighbours = transformed(currentWord, set);
+                for(String word : neighbours){
+                    visited.add(word);
+                    if(!adj.containsKey(word)){
+                        adj.put(word, new ArrayList<>());
+                    }
+                    adj.get(word).add(currentWord);
+                    if(!isEnqueued.containsKey(word)){
+                        isEnqueued.put(word, 1);
+                        queue.add(word);
+                    }
+                }
+            }
+            for(int i = 0; i < visited.size(); i++){
+                if(set.contains(visited.get(i))){
+                    set.remove(visited.get(i));
+                }
+            }
+        }
+    }
+    public void backtrack(String word, String endWord, List<String> list){
+        //System.out.println(word);
+        if(word.equals(endWord)){
+            List<String> temp = new ArrayList<>(list);
+            Collections.reverse(temp);
+            result.add(new ArrayList<>(temp));
+        }
+        if(!adj.containsKey(word)){
+            return;
+        }
+        for(String s : adj.get(word)){
+            list.add(s);
+            backtrack(s, endWord, list);
+            list.remove(list.size() - 1);
+        }
+    }
+    public List<String> transformed(String s, Set<String> set){
+        List<String> list = new ArrayList<>();
+        char word[] = s.toCharArray();
+        for(int i = 0; i < s.length(); i++){
+            char old = word[i];
+            for(char c = 'a'; c <= 'z'; c++){
+                word[i] = c;
+                if(c != old && set.contains(String.valueOf(word))){
+                    list.add(String.valueOf(word));
+                }
+            }
+            word[i] = old;
+        }
+        return list;
+    }
+}
