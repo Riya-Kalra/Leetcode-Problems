de49ded7c286f934557959ec8fe08a7f066b894d
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-14 11:48:11 +0530

    Time: 35 ms (6.31%), Space: 49.6 MB (25.23%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.java
@@ -6,13 +6,11 @@ class Solution {
             area[i][1]= Math.min(n,i+range[i]);
         }
         Arrays.sort(area,(a,b)->(a[0]-b[0]));
-        
-        
-        
+         
         int count = 0, start_time = 0, end_time = 0, i = 0;
         
         while (end_time < n) {
-            for (; i <= n && area[i][0] <= start_time; i++) {
+            for (i=0 ; i <= n && area[i][0] <= start_time; i++) {
                 end_time = Math.max(end_time, area[i][1]);
             }
             if (start_time == end_time) return -1;
