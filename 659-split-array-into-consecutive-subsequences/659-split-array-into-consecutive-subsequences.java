e76fd73f9af780325dd2fdf5245b51779b022552
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-19 10:19:04 +0530

    Time: 35 ms (58.64%), Space: 72.9 MB (11.82%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -6,22 +6,23 @@ class Solution {
         for(int n:nums)
             map.put(n,map.getOrDefault(n,0)+1);
         for (int n :nums){
-            if(map.containsKey(n) && map.get(n)>0 ){
+            if(map.containsKey(n)){
                 map.put(n,map.get(n)-1);
-                if(map.get(n)<0) map.remove(n);
-                if(end.containsKey(n-1) && end.get(n-1)>0){
+                if(map.get(n)<=0) map.remove(n);
+                
+                if(end.containsKey(n-1) ){
                     end.put(n,end.getOrDefault(n,0)+1);
                     end.put(n-1,end.get(n-1)-1);
-                    if(end.get(n-1)<0) end.remove(n-1);
+                    if(end.get(n-1)<=0) end.remove(n-1);
                     
                 }
-                else if (map.containsKey(n+1) && map.containsKey(n+2)&& map.get(n+1)>0 && map.get(n+2)>0){
+                else if (map.containsKey(n+1) && map.containsKey(n+2)){
                     
                     end.put(n+2,end.getOrDefault(n+2,0)+1);
                     map.put(n+1,map.get(n+1)-1);
                     map.put(n+2,map.get(n+2)-1);
-                    if(map.get(n+1)<0) map.remove(n+1);
-                    if(map.get(n+2)<0) map.remove(n+2);
+                    if(map.get(n+1)<=0) map.remove(n+1);
+                    if(map.get(n+2)<=0) map.remove(n+2);
                     
                 }
                 else
