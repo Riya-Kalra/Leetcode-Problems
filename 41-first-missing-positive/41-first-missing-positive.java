582816419b6a73a428175cb022d1bf2a26c3c5c8
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-19 10:29:23 +0530

    Time: 287 ms (5.09%), Space: 166.6 MB (16.33%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -2,6 +2,7 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         TreeSet <Integer> set = new TreeSet<>();
         for(int n: nums)
+            if(n>0)
             set.add(n);
         int i=1;
         while(true){
