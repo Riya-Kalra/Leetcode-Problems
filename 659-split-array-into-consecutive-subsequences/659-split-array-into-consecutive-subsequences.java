1d2821c4d23748c927f22663952d9c3991ddb804
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-19 10:16:08 +0530

    Time: 60 ms (23.18%), Space: 71.8 MB (45.23%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isPossible(int[] nums) {
+        HashMap <Integer,Integer> map =new HashMap<>();// store count of each ele
+        HashMap <Integer,Integer> end =new HashMap<>();// no of subsequences ending with this ele
+        
+        for(int n:nums)
+            map.put(n,map.getOrDefault(n,0)+1);
+        for (int n :nums){
+            if(map.containsKey(n) && map.get(n)>0 ){
+                map.put(n,map.get(n)-1);
+                
+                if(end.containsKey(n-1) && end.get(n-1)>0){
+                    end.put(n,end.getOrDefault(n,0)+1);
+                    end.put(n-1,end.get(n-1)-1);
+                    
+                }
+                else if (map.containsKey(n+1) && map.containsKey(n+2)&& map.get(n+1)>0 && map.get(n+2)>0){
+                    
+                    end.put(n+2,end.getOrDefault(n+2,0)+1);
+                    map.put(n+1,map.get(n+1)-1);
+                    map.put(n+2,map.get(n+2)-1);
+                    
+                }
+                else
+                    return false;
+                
+            }
+            //System.out.println(map);
+            //System.out.println(" end "+ end);
+        }
+        return true;
+    }
+}
