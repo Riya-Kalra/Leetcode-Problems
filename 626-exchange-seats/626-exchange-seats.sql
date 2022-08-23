e5c6d665c01436e4625d967d91be9466f8205c90
Riya-Kalra <81101962+Riya-Kalra@users.noreply.github.com>
2022-08-23 10:34:17 +0530

    Time: 446 ms (40.35%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 SELECT
 	CASE
-		WHEN seat.id % 2 != 0 AND seat.id = (SELECT COUNT(*) FROM seat) THEN seat.id
 		WHEN seat.id % 2 = 0 THEN seat.id - 1
+        WHEN seat.id = (SELECT COUNT(*) FROM seat) THEN seat.id
 		ELSE
 			seat.id + 1
 	END as id,
